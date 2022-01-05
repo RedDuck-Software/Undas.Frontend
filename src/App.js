1d7a992fe7e3aef4cc2455e501a8402763a4dd5f
standin-glhf <bogdan.voloshko@gmail.com>
2022-01-05 16:23:40 +0200

    pull fix

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ const App = () => {
   useEffect(() => {
     connectOnLoad(web3Current);
   }, []);
+  console.log(`here's your address: ${web3Current.account}`);
   return (
     <>
       <Navbar />
