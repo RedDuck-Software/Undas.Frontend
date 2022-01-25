27e40243c6c6d9b44a5aecd483eb30c9438c4f6d
Andrii <shvaika.andrii@gmail.com>
2022-01-25 10:08:25 +0200

    changed code of transactions and completed 1 task

---
+++ src/utils/calculateTerm.ts
@@ -0,0 +1,6 @@
+export const calculateTerm = (interval: number) => {
+  const date = new Date().valueOf();
+  const termInSeconds = Math.abs(interval - date) / 999;
+  const term = Math.floor(termInSeconds / 86400);
+  return term;
+};
