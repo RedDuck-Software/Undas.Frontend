aef9a1289f85184fe682c512f97faed6c357ecb3
glxck <dim4ik3013@gmail.com>
2022-03-21 15:12:16 +0200

    Create collection as a Component

--- src/pages/CollectionsPage/Collections.tsx
+++ src/pages/CollectionsPage/Collections.tsx
@@ -27,6 +27,7 @@ import {
     CollectionPic
 } from "./imports";
 import { Container } from "../../globalStyles";
+import Collection from "./page-components/Collection";
 
 const Collections:FC = () => {
     return (
@@ -52,39 +53,10 @@ const Collections:FC = () => {
                     </InfoCard>
                 </Info>
                 <div>
-                    <CollectionCard>
-                        <CollectionBackground src={CollectionBG} alt="collection-bg"/>
-                        <AuthorWrap>
-                            <CollectionPicWrap>
-                                <img src={CollectionPic} alt="collection-pic"/>
-                            </CollectionPicWrap>
-                            {/*CollectionsPage name*/}
-                            <div>
-                                <Wrapper disp="flex" alignItems="center" gap="10px" marg="0 20px 0 0">
-                                    <CollectionText fs="14px">Borya Borya</CollectionText>
-                                    <img src={Verified} alt="verified-ico"/>
-                                    <Platform>UND</Platform>
-                                </Wrapper>
-                                <CollectionText>by Borya Borya</CollectionText>
-                            </div>
-                            <Wrapper disp="flex" mw="206px" marg="0 0 0 auto">
-                                <CollectionText lh="15px" padd="5px 0">
-                                    If you believe in the future of DeFi, then you believein the future of rekt.news. Each auction winner will also be sent ...
-                                </CollectionText>
-                            </Wrapper>
-                        </AuthorWrap>
-                        <Wrapper disp="flex" gap="15px">
-                            <NFTCards>
-                                <img src={NFT1} alt="nft-card"/>
-                            </NFTCards>
-                            <NFTCards>
-                                <img src={NFT2} alt="nft-card"/>
-                            </NFTCards>
-                            <NFTCards>
-                                <img src={NFT3} alt="nft-card"/>
-                            </NFTCards>
-                        </Wrapper>
-                    </CollectionCard>
+                    <Collection />
+                    <Collection />
+                    <Collection />
+                    <Collection />
                 </div>
             </Container>
         </>
