b7a306f5283228a6d7de256eb1e0e8d10f69ee85
standin-glhf <bogdan.voloshko@gmail.com>
2022-04-07 02:18:46 +0300

    user's NFTs output

--- src/pages/AccountPage/AccountPage.tsx
+++ src/pages/AccountPage/AccountPage.tsx
@@ -17,45 +17,44 @@ import ASideFilter from "../../components/ASideFilter/ASideFilter";
 
 //Media
 import {
-    card01,
-    card02,
-    card03,
-    card04,
-    AccountBanner,
-    CreatedIco,
-    OffersIco,
-    FavouriteIco,
-    RewardIco,
-    ReferralIco,
+  card01,
+  card02,
+  card03,
+  card04,
+  AccountBanner,
+  CreatedIco,
+  OffersIco,
+  FavouriteIco,
+  RewardIco,
+  ReferralIco,
 } from "./imports";
 //Styles
 import { Container, Background, Button } from "../../globalStyles";
-import {Banner} from "../CategoriesPage/Categories.styles";
-import {Wrapper} from "../CategoriesPage/Categories.styles";
+import { Banner } from "../CategoriesPage/Categories.styles";
+import { Wrapper } from "../CategoriesPage/Categories.styles";
 import {
-    AccountContainer,
-    TabsMenu,
-    Tab,
-    SmallNumber
+  AccountContainer,
+  TabsMenu,
+  Tab,
+  SmallNumber,
 } from "./AccountPage.styles";
 import {
-    Arrow,
-    Filter,
-    FilterItem,
-    FilterMenu,
-    FilterTitle,
-    GridLayout,
-    Input,
-    MenuItem,
-    MenuSearchWrap,
-    MenuWrap,
-    SearchIco,
-    SettingsBlock,
-    SettingsElement,
-    ViewButton,
-    ViewOption
+  Arrow,
+  Filter,
+  FilterItem,
+  FilterMenu,
+  FilterTitle,
+  GridLayout,
+  Input,
+  MenuItem,
+  MenuSearchWrap,
+  MenuWrap,
+  SearchIco,
+  SettingsBlock,
+  SettingsElement,
+  ViewButton,
+  ViewOption,
 } from "../AllNFTs/AllNFTs.styles";
-
 
 import { useWeb3React } from "@web3-react/core";
 import Cookies from "universal-cookie";
@@ -64,21 +63,20 @@ import { operations } from "moralis/types/generated/web3Api";
 import { useMoralis } from "react-moralis";
 import { Navigate } from "react-router-dom";
 import NFTGrid from "../../components/NFTCard/Grid/NFTGrid";
-import {GridIco, ListIco} from "../AllNFTs/imports";
+import { GridIco, ListIco } from "../AllNFTs/imports";
 import MainMenu from "./page-components/MainMenu/MainMenu";
 import FavouriteMenu from "./page-components/MainMenu/FavouriteMenu";
 import OffersMenu from "./page-components/MainMenu/OffersMenu";
 
 const AccountPage = () => {
-    const [active, setActive] = useState<any>({
-        price: false,
-        event: false,
-    });
-    const [tab, setTab] = useState('offer')
-    console.log(tab)
+  const [active, setActive] = useState<any>({
+    price: false,
+    event: false,
+  });
+  const [tab, setTab] = useState("offer");
+  console.log(tab);
   const cookies = new Cookies();
   let { account, deactivate } = useWeb3React();
-
 
   const { connector } = useContext(Context);
   const { Moralis } = useMoralis();
@@ -109,9 +107,7 @@ const AccountPage = () => {
     return listOfNFTS;
   };
 
-  const getAccountData = async () => {
-
-  }
+  const getAccountData = async () => {};
   const getListData = async () => {
     const response = await getNFTList();
     if (!response?.result) return;
@@ -142,46 +138,57 @@ const AccountPage = () => {
 
   return (
     <>
-        <Wrapper>
-          <Banner>
-            <img src={AccountBanner} alt="account-banner"/>
-          </Banner>
-            <AccountContainer>
-            <AccountCard account={account} disconnect={disconnect}/>
-                {tab !== 'favourite' && <ASideFilter marginTop="140px"/>}
-                <Wrapper w="100%">
-                    <Wrapper w="100%" marg="15px 0 0 0">
-                            <TabsMenu>
-                                <Tab onClick={() => setTab('')}>
-                                    <CreatedIco/>
-                                    <span>Created</span>
-                                    <SmallNumber>2</SmallNumber>
-                                </Tab>
-                                <Tab onClick={() => setTab('offers')}>
-                                    <OffersIco/>
-                                    <span>Offers</span>
-                                    <SmallNumber>6</SmallNumber>
-                                </Tab>
-                                <Tab onClick={() => setTab('favourite')}>
-                                    <FavouriteIco/>
-                                    <span>Favourite</span>
-                                </Tab>
-                                <Tab>
-                                    <RewardIco/>
-                                    <span>Reward</span>
-                                </Tab>
-                                <Tab>
-                                    <ReferralIco/>
-                                    <span>Referral</span>
-                                </Tab>
-                            </TabsMenu>
-                    </Wrapper>
-                    {tab === '' && <MainMenu />}
-                    {tab === 'favourite' && <FavouriteMenu />}
-                    {tab === 'offers' && <OffersMenu />}
-                </Wrapper>
-            </AccountContainer>
-        </Wrapper>
+      <Wrapper>
+        <Banner>
+          <img src={AccountBanner} alt="account-banner" />
+        </Banner>
+        <AccountContainer>
+          <AccountCard account={account} disconnect={disconnect} />
+          {tab !== "favourite" && <ASideFilter marginTop="140px" />}
+          <Wrapper w="100%">
+            <Wrapper w="100%" marg="15px 0 0 0">
+              <TabsMenu>
+                <Tab onClick={() => setTab("")}>
+                  <CreatedIco />
+                  <span>Created</span>
+                  <SmallNumber>2</SmallNumber>
+                </Tab>
+                <Tab onClick={() => setTab("offers")}>
+                  <OffersIco />
+                  <span>Offers</span>
+                  <SmallNumber>6</SmallNumber>
+                </Tab>
+                <Tab onClick={() => setTab("favourite")}>
+                  <FavouriteIco />
+                  <span>Favourite</span>
+                </Tab>
+                <Tab>
+                  <RewardIco />
+                  <span>Reward</span>
+                </Tab>
+                <Tab>
+                  <ReferralIco />
+                  <span>Referral</span>
+                </Tab>
+              </TabsMenu>
+            </Wrapper>
+            {tab === "" && <MainMenu />}
+            {tab === "favourite" && <FavouriteMenu />}
+            {tab === "offers" && <OffersMenu />}
+            <GridLayout>
+              {NFTList?.map((item) => {
+                return (
+                  <NFTGrid
+                    tokenId={+item.token_id}
+                    URI={item.token_uri!}
+                    name={item.name}
+                  />
+                );
+              })}
+            </GridLayout>
+          </Wrapper>
+        </AccountContainer>
+      </Wrapper>
     </>
   );
 };
