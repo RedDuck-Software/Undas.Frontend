ae7985c03ce133f2b1f49ac212a3a715b1fbc2f5
Alisa <a.fedko@student.csn.khai.edu>
2022-05-04 17:39:07 +0300

    Home Page Slogan, buttons, banner

--- src/pages/HomePage/page-components/Slider/imports.ts
+++ src/pages/HomePage/page-components/Slider/imports.ts
@@ -1,5 +1,7 @@
 import SliderBG from '../../../../images/image-slider/slider-image.png'
+import CollectionPrev from "../../../../images/image-home/collectionImg.svg";
 
 export {
-    SliderBG
+    SliderBG,
+    CollectionPrev
 }
