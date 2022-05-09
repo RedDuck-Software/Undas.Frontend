356218b921a60d08b339394cb37c791624b36ef4
Gurin Alexandr <gurinalex91@gmail.com>
2022-05-09 16:11:58 +0300

    add Earnings settings

---
+++ src/pages/Settings/SettingsTabs/Earnings/Earnings.styles.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const EarningsWrap = styled.div `
+  padding-bottom: 80px;
+`;
+
+export const EarningsContentWrap = styled.div `
+  padding: 20px 30px 40px;
+  background: #FBF5FF;
+  box-shadow: inset 0px 0px 1px #7C7C7C;
+  border-radius: 5px;
+  margin: 40px auto;
+`;
+
+export const EarningsContentText = styled.p`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
+  color: #232428;
+  & a{
+    color: #873DC1;
+    text-decoration: none;
+  }
+`;
+export const EarningsContentMessage = styled.p`
+  text-align: center;
+  padding: 15px;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 17px;
+  color: #232428;
+  background: #FFFFFF;
+  border: 1px solid rgba(135, 61, 193, 0.25);
+  box-sizing: border-box;
+  border-radius: 10px;
+`;
+
