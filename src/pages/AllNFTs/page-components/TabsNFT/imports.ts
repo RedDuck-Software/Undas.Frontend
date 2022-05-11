fcb0a5c0cec272a78abf7b3d0cbb572d6bf28fe4
Alisa <a.fedko@student.csn.khai.edu>
2022-05-11 15:25:13 +0300

    All NFT List + fix .css

--- src/pages/AllNFTs/page-components/TabsNFT/imports.ts
+++ src/pages/AllNFTs/page-components/TabsNFT/imports.ts
@@ -1,22 +1,8 @@
 
 import ethereum from '../../../../images/image-AllNFTPage/ethereum.svg'
-import NFTModal from '../../../../images/image-AllNFTPage/NFTModal.svg'
-import collection from '../../../../images/image-AllNFTPage/collection.svg'
-import imageNft from '../../../../images/image-AllNFTPage/image-nft.svg'
-import und from '../../../../images/image-AllNFTPage/und.svg'
-import btnLeft from '../../../../images/image-AllNFTPage/btnLeft.svg'
-import btnRight from '../../../../images/image-AllNFTPage/btnRight.svg'
 import time from '../../../../images/image-AllNFTPage/time.svg'
-import hourglass from '../../../../images/image-AllNFTPage/hourglass-split.svg'
 
 export {
     ethereum,
-    NFTModal,
-    collection,
-    imageNft,
-    und,
-    btnLeft,
-    btnRight,
     time,
-    hourglass,
 }
