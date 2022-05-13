f3e3d03aaac5a7f2317f34cb4f05ba85651294dc
Alisa <a.fedko@student.csn.khai.edu>
2022-05-13 20:58:20 +0300

    Achievements Not Full

--- src/pages/AccountPage/imports.ts
+++ src/pages/AccountPage/imports.ts
@@ -7,6 +7,28 @@ import AccountBanner from './assets/account-banner.png'
 import EthBlack from './assets/eth-black.svg'
 import filter from './assets/filter-photo.svg'
 import close from './assets/close.svg'
+import dollarReward from './assets/dollar-reward.svg'
+import caseReward from './assets/case-reward.svg'
+import ethReward from './assets/eth-reward.svg'
+import moneyReward from './assets/money-reward.svg'
+import peopleReward from './assets/people-reward.svg'
+import bronzeDollarReward from './assets/bronze-dollar.svg'
+import bronzeCaseReward from './assets/bronze-case.svg'
+import bronzeEthReward from './assets/bronze-eth.svg'
+import bronzeMoneyReward from './assets/bronze-money.svg'
+import bronzePeopleReward from './assets/bronze-people.svg'
+import silverDollarReward from './assets/silver-dollar.svg'
+import silverCaseReward from './assets/silver-case.svg'
+import silverEthReward from './assets/silver-eth.svg'
+import silverMoneyReward from './assets/silver-money.svg'
+import silverPeopleReward from './assets/silver-people.svg'
+import goldDollarReward from './assets/gold-dollar.svg'
+import goldCaseReward from './assets/gold-case.svg'
+import goldEthReward from './assets/gold-eth.svg'
+import goldMoneyReward from './assets/gold-money.svg'
+import goldPeopleReward from './assets/gold-people.svg'
+import backImg from './assets/back-img.svg'
+import diamond from './assets/diamond.svg'
 import {ReactComponent as Verified} from '../../images/image-collection/verified.svg'
 import {ReactComponent as Copy} from './assets/copy-ico.svg'
 import {ReactComponent as SingOut} from './assets/sign-out.svg'
@@ -71,4 +93,26 @@ export { card01,
     EthBlack,
     filter,
     close,
+    dollarReward,
+    caseReward,
+    ethReward,
+    moneyReward,
+    peopleReward,
+    bronzeDollarReward,
+    bronzeCaseReward,
+    bronzeEthReward,
+    bronzeMoneyReward,
+    bronzePeopleReward,
+    silverDollarReward,
+    silverCaseReward,
+    silverEthReward,
+    silverMoneyReward,
+    silverPeopleReward,
+    goldDollarReward,
+    goldCaseReward,
+    goldEthReward,
+    goldMoneyReward,
+    goldPeopleReward,
+    backImg,
+    diamond,
 }
--- src/pages/AccountPage/page-components/Achievements/Achievements.styles.ts
+++ src/pages/AccountPage/page-components/Achievements/Achievements.styles.ts
@@ -1,8 +1,180 @@
 import styled from "styled-components";
 
 export const AchievementsWrap = styled.div `
-  width: 50px;
-  height: 50px;
-  overflow: hidden;
-  background: #123;
-`;
+
+`
+export const AchievementsText = styled.div `
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 400;
+font-size: 24px;
+line-height: 29px;
+text-align: center;
+color: #232428;
+margin-top: 60px;
+display: inline-block;
+text-align: left;
+width: 50%;
+`
+export const InfoText = styled.div `
+width: 493px;
+height: 74px;
+background: #FBF5FF;
+box-shadow: 0px 4px 10px rgba(135, 61, 193, 0.25);
+background: FBF5FF;
+border-radius: 10px;
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 17px;
+color: #232428;
+margin-top: 60px;
+display: inline-block;
+text-align: left;
+width: 50%;
+max-width: 500px;
+padding: 20px;
+float: right;
+`
+export const PurpleRewards = styled.div `
+filter: drop-shadow(0px 15px 15px #873DC1);
+box-shadow: inset 0px 0px 1px #7C7C7C;
+border-radius: 10px 10px 10px 10px;
+height: 200px;
+background: #FFFFFF;
+margin-top: 80px;
+`
+export const PurpleRewardsTitle = styled.div `
+background: #CCB7F2;
+box-shadow: inset 0px 0px 1px #7C7C7C;
+border-radius: 10px 10px 0px 0px;
+height: 50px;
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 22px;
+text-align: center;
+color: #232428;
+padding-top: 14px;
+`
+export const RewardsDiv = styled.div `
+height: 100px;
+width: 320px;
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 22px;
+color: #232428;
+padding: 20px;
+margin-right: 210px;
+display: inline-block;
+`
+export const RewardImg = styled.img `
+margin-bottom: 40px;
+`
+export const BronzeRewards = styled.div `
+filter: drop-shadow(0px 15px 15px #CD7F32);
+box-shadow: inset 0px 0px 1px #CD7F32;
+border-radius: 10px 10px 10px 10px;
+height: 200px;
+background: #FFFFFF;
+margin-top: 40px;
+`
+export const BronzeRewardsTitle = styled.div `
+background: #CD7F32;
+box-shadow: inset 0px 0px 1px #CD7F32;
+border-radius: 10px 10px 0px 0px;
+height: 50px;
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 22px;
+text-align: center;
+color: #232428;
+padding-top: 14px;
+`
+export const RewardImgBronze = styled.img `
+margin-bottom: 70px;
+`
+export const SilverRewards = styled.div `
+filter: drop-shadow(0px 15px 15px #C0C0C0);
+box-shadow: inset 0px 0px 1px #C0C0C0;
+border-radius: 10px 10px 10px 10px;
+height: 200px;
+background: #FFFFFF;
+margin-top: 40px;
+`
+export const SilverRewardsTitle = styled.div `
+background: #C0C0C0;
+box-shadow: inset 0px 0px 1px #C0C0C0;
+border-radius: 10px 10px 0px 0px;
+height: 50px;
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 22px;
+text-align: center;
+color: #232428;
+padding-top: 14px;
+`
+export const GoldRewards = styled.div `
+filter: drop-shadow(0px 15px 15px #FFD700);
+box-shadow: inset 0px 0px 1px #FFD700;
+border-radius: 10px 10px 10px 10px;
+height: 200px;
+background: #FFFFFF;
+margin-top: 40px;
+`
+export const GoldRewardsTitle = styled.div `
+background: #FFD700;
+box-shadow: inset 0px 0px 1px #FFD700;
+border-radius: 10px 10px 0px 0px;
+height: 50px;
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 22px;
+text-align: center;
+color: #232428;
+padding-top: 14px;
+`
+export const BackImg = styled.img `
+position: absolute;
+margin-top: 300px;
+margin-left: -120px;
+`
+export const Diamond = styled.div `
+margin-top: 40px;
+height: 120px;
+background: #CCB7F2;
+box-shadow: 0px 0px 10px rgba(135, 61, 193, 0.25), inset 0px 0px 1px #7C7C7C;
+border-radius: 10px;
+`
+export const DiamondImg = styled.img `
+margin-left: 30px;
+margin-top: 10px;
+`
+export const DiamondText = styled.span `
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 22px;
+color: #FFFFFF;
+margin-left: 30px;
+`
+export const UndasText = styled.span `
+font-family: 'Jost';
+font-style: normal;
+font-weight: 600;
+font-size: 18px;
+line-height: 26px;
+color: #873DC1;
+margin-left: 6px;
+`
