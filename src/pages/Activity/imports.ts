498d745f81d1d8865c8c7a3db93b75caeebc8a8b
Alisa <a.fedko@student.csn.khai.edu>
2022-05-13 18:51:22 +0300

    Second Merge
    NabBar исправлен z-index
    AccountPage Favorite добавлено отображение NFT списком.
    Добавлена таблица Favorite Collections
    
    Очищена от лишних вложений страница AllNFT
    
    TopCollection добавлен фильтр (и элементы для удаления выбранных фильтров)
    
    Activity - добавлены элементы для удаления выбранных фильтров

--- src/pages/Activity/imports.ts
+++ src/pages/Activity/imports.ts
@@ -1,7 +1,11 @@
 import ItemImg from '../../images/item-icon.png'
+import filter from './assets/filter-photo.svg'
+import close from './assets/close.svg'
 import {ReactComponent as ItemVerifyIco} from './assets/verify-ico.svg'
 
 export { 
     ItemImg,
     ItemVerifyIco,
+    filter,
+    close,
 }
--- src/pages/TopCollectionPage/imports.ts
+++ src/pages/TopCollectionPage/imports.ts
@@ -1,6 +1,8 @@
 import ItemImg from '../../images/item-icon.png'
 import top from './assets/top.svg'
 import buttom from './assets/buttom.svg'
+import filter from './assets/filter-photo.svg'
+import close from './assets/close.svg'
 import {ReactComponent as ItemVerifyIco} from './assets/verify-ico.svg'
 
 export { 
@@ -8,4 +10,6 @@ export {
     ItemVerifyIco,
     top,
     buttom,
+    close,
+    filter,
 }
