2aa63fd39e8d702d980b9156f95ce814c060c174
Gurin Alexandr <gurinalex91@gmail.com>
2022-06-15 16:04:47 +0300

    adaptive blog main page

--- src/pages/Blog/Blog.styles.ts
+++ src/pages/Blog/Blog.styles.ts
@@ -3,13 +3,13 @@ import styled from "styled-components";
 export const BlogWrap = styled.div`
   padding: 80px 0 120px;
   @media (max-width: 992px) {
-    padding: 60px 0 80px;
+    padding: 40px 0 80px;
   }
   @media (max-width: 768px) {
-    padding: 40px 0 60px;
+    padding: 10px 0 60px;
   }
   @media (max-width: 576px) {
-    padding: 20px 0 30px;
+    padding: 0 0 30px;
   }
 `;
 export const Text = styled.p`
@@ -20,11 +20,21 @@ export const Text = styled.p`
   font-size: 16px;
   line-height: 20px;
   color: #7c7c7c;
+  @media (max-width: 992px) {
+    width: 80%;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-bottom: 20px;
+  }
 `;
 export const TabsMenuWrap = styled.div`
   background-color: #fff;
   box-shadow: 0px 0px 1px #7c7c7c;
   margin: 40px 0;
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 export const TabsMenu = styled.ul`
   display: flex;
@@ -47,6 +57,9 @@ export const Tab = styled.li`
     cursor: pointer;
     color: #873dc1;
     border-bottom: 4px solid rgba(135, 61, 193, 0.5);
+  }
+  @media (max-width: 992px) {
+    padding: 15px 20px 11px;
   }
 `;
 export const BlogTitle = styled.h2`
--- src/pages/Blog/page-components/BlogMainGrid/BlogMainGrid.styles.ts
+++ src/pages/Blog/page-components/BlogMainGrid/BlogMainGrid.styles.ts
@@ -5,4 +5,13 @@ export const Wrap = styled.div`
   grid-template-columns: repeat(3, 1fr);
   grid-auto-rows: 340px;
   gap: 30px;
+  @media (max-width: 768px) {
+    grid-auto-rows: 300px;
+    gap: 15px;
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: 576px) {
+    gap: 10px;
+    display: none;
+  }
 `;
