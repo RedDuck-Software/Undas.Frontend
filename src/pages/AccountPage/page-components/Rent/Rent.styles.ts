b0272d04a113ca9b5587a52962dd91f87cae5ed7
Gurin Alexandr <gurinalex91@gmail.com>
2022-06-22 16:38:08 +0300

    add content to Rent tab Account Page

--- src/pages/AccountPage/page-components/Rent/Rent.styles.ts
+++ src/pages/AccountPage/page-components/Rent/Rent.styles.ts
@@ -1,24 +1,32 @@
 import styled from "styled-components";
 
-import {
-    ViewOption,
-    ResultsTotal,
-    SettingsBlock,
-  } from "../../../AllNFTs/AllNFTs.styles";
+import { ResultsTotal, SettingsBlock } from "../../../AllNFTs/AllNFTs.styles";
 
-export const FavouriteSettingsBlock = styled(SettingsBlock)`
+export const RentWrap = styled.div`
+  padding-bottom: 80px;
+  @media (max-width: 992px) {
+    padding-bottom: 40px;
+  }
+  @media (max-width: 768px) {
+    padding-bottom: 20px;
+  }
+  @media (max-width: 576px) {
+    padding-bottom: 0;
+  }
+`;
+
+export const RentSettingsBlock = styled(SettingsBlock)`
   @media (max-width: 768px) {
     flex: 0 0 100%;
   }
 `;
-export const FavouriteSelect = styled.div`
+export const RentSelect = styled.div`
   box-shadow: 0px 0px 5px rgba(135, 61, 193, 0.25);
   border-radius: 10px;
   height: 36px;
   overflow: hidden;
   display: flex;
   align-items: center;
-  min-width: 400px;
   @media (max-width: 768px) {
     width: 100%;
   }
@@ -26,7 +34,7 @@ export const FavouriteSelect = styled.div`
     min-width: 100%;
   }
 `;
-export const FavouriteButton = styled.button`
+export const RentButton = styled.button`
   cursor: pointer;
   background: #ffffff;
   height: 40px;
@@ -39,7 +47,7 @@ export const FavouriteButton = styled.button`
   &:hover {
     background-color: #fbf5ff;
   }
-  &.favourite-active {
+  &.rent-active {
     background-color: #edd2ff;
   }
   @media (max-width: 768px) {
@@ -52,7 +60,7 @@ export const FavouriteButton = styled.button`
     padding: 8px;
   }
 `;
-export const FavouriteResultsTotal = styled(ResultsTotal)`
+export const RentResultsTotal = styled(ResultsTotal)`
   @media (max-width: 1200px) {
     flex: 0 0 20%;
     text-align: right;
