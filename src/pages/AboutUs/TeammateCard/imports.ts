7027a2a279c1d98b4f70371ff9be469e17e8dea8
Alisa <a.fedko@student.csn.khai.edu>
2022-06-27 17:44:57 +0300

    Add photo developers  + Fix adaptive (Create NFT+Create Collections)

--- src/pages/AboutUs/TeammateCard/imports.ts
+++ src/pages/AboutUs/TeammateCard/imports.ts
@@ -1,6 +1,33 @@
 import LinkedInIco from "../../../icons/linkedin.svg";
 import TwitterIco from "../../../icons/twitter.svg";
-import CardImg1 from "../../../images/image-cards/TeamCards/card1.png";
-import CardImg2 from "../../../images/image-cards/TeamCards/card2.png";
+import GermanFounder from "./assets/GermanSolovyov.jpeg";
+import ErvinMigirov from "./assets/ErvinMigirov.jpg";
+import ArtemFranz from "./assets/ArtemFranz.jpeg";
+import MarkFounder from "./assets/MarkFounder.jpeg";
+import StasnislavStefanyshynCVO from "./assets/StasnislavStefanyshynCVO.jpg";
+import AlexandrGurinDeveloper from "./assets/AlexandrGurinDeveloper.jpg";
+import AlisaFedkoDeveloper from "./assets/AlisaFedkoDeveloper.jpg";
+import KostiantynMininkov from "./assets/KostiantynMininkov.jpg";
+import DanilDeveloper from "./assets/DanilDeveloper.jpg";
+import AndriiShvaika from "./assets/AndriiShvaika.jpg";
+import MaximKulikovDesigner from "./assets/MaximKulikovDesigner.jpg";
+import AnnaTsysMetaverses from "./assets/AnnaTsysMetaverses.jpg";
+import IlyaArkhipovArtDirector from "./assets/IlyaArkhipovArtDirector.jpg";
 
-export { CardImg1, CardImg2, LinkedInIco, TwitterIco };
+export {
+  LinkedInIco,
+  TwitterIco,
+  GermanFounder,
+  ErvinMigirov,
+  ArtemFranz,
+  MarkFounder,
+  StasnislavStefanyshynCVO,
+  AlexandrGurinDeveloper,
+  AlisaFedkoDeveloper,
+  KostiantynMininkov,
+  AndriiShvaika,
+  MaximKulikovDesigner,
+  AnnaTsysMetaverses,
+  IlyaArkhipovArtDirector,
+  DanilDeveloper,
+};
