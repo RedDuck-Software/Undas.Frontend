94979fdbc0548339353ba1b6fc752d415eb9a77f
gurinalex91 <gurinalex91@gmail.com>
2022-07-15 13:24:48 +0300

    add error modal

---
+++ src/components/Modal/Error/Error.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
+
+import { Wrap, StyledButton, StyledModal, StyledHeader, StyledBody, StyledFooter } from "./Error.styles";
+
+const Error: React.FC = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <Wrap>
+      <StyledButton onClick={handleShow}>
+        Launch modal
+      </StyledButton>
+      <StyledModal show={show} onHide={handleClose}>
+        <StyledHeader closeButton>
+          <Modal.Title>Oops...Something went wrong</Modal.Title>
+        </StyledHeader>
+        <StyledBody>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti doloribus rem iste minima, cum reprehenderit dicta.</StyledBody>
+        <StyledFooter>
+          <StyledButton onClick={handleClose}>
+            OK
+          </StyledButton>
+        </StyledFooter>
+      </StyledModal>
+    </Wrap>
+  );
+};
+
+export default Error;
