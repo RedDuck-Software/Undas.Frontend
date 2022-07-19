4609c0620547663c3f0550b8568fdaf0ffd4cfc9
gurinalex91 <gurinalex91@gmail.com>
2022-07-19 18:39:05 +0300

    made the error modal bigger

--- src/components/Modal/Error/Error.styles.ts
+++ src/components/Modal/Error/Error.styles.ts
@@ -26,6 +26,11 @@ export const StyledModal = styled(Modal)`
   & .modal-content {
     border-radius: 20px;
   }
+  @media (min-width: 576px) {
+    & .modal-dialog {
+      max-width: 50%;
+    }
+  }
 `;
 export const StyledHeader = styled(Modal.Header)`
   background: #ebdfff;
