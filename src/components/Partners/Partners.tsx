b6c245a74ba71df47c3393ab36f58ab14ebc6200
gurinalex91 <gurinalex91@gmail.com>
2022-07-20 12:40:14 +0300

    create partner component + add home page and about page

---
+++ src/components/Partners/Partners.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import {
+  Wrap,
+  PartnersWrap,
+  FirstPartner,
+  SecondPartner,
+} from "./Partners.styles";
+
+import { Container } from "../../globalStyles";
+
+const Partners: React.FC = () => {
+  return (
+    <Wrap>
+      <Container>
+        <PartnersWrap>
+          <FirstPartner
+            href="https://redduck.io/"
+            target="_blank"
+          ></FirstPartner>
+
+          <SecondPartner
+            href="#"
+            target="_blank"
+          ></SecondPartner>
+        </PartnersWrap>
+      </Container>
+    </Wrap>
+  );
+};
+
+export default Partners;
