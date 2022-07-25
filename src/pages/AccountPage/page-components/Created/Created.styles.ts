1ad4bba643cbef50359d68750a446c474a42a831
gurinalex91 <gurinalex91@gmail.com>
2022-07-25 12:56:29 +0300

    fix img object-fit

--- src/pages/AccountPage/page-components/Created/Created.styles.ts
+++ src/pages/AccountPage/page-components/Created/Created.styles.ts
@@ -147,6 +147,7 @@ export const CollectionPicWrap = styled.div`
     width: 100%;
     height: 100%;
     border-radius: 20px;
+    object-fit: cover;
   }
   @media (max-width: 576px) {
     height: 60px;
